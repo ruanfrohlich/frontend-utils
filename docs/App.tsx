e51eb511f3bfb1235214020b7f2f6760ae6d7fb1
Ruan Frohlich <ruanfrohlich@gmail.com>
2024-10-22 21:38:49 -0300

    Ajustes para docs

---
+++ docs/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'components';
+
+export const App = () => {
+  return (
+    <main className="site-main">
+      <div className="wrapper">
+        <Button />
+      </div>
+    </main>
+  );
+};
---
+++ docs/index.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const appElement = document.getElementById('app');
+
+if (appElement) {
+  const root = createRoot(appElement);
+  root.render(<App />);
+}
