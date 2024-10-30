88d82e2a9f2576a3fac8d35647c023941e71d37f
Ruan Frohlich <ruanfrohlich@gmail.com>
2024-10-30 14:30:07 -0300

    Ajustes stories

--- preview/Button.stories.ts
+++ preview/Button.stories.ts
@@ -14,7 +14,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Preview: Story = {
   args: {
     children: 'Button',
   },
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,14 @@ import React, { ReactNode } from 'react';
 
 interface IButton {
   children: ReactNode;
+  visual?: 'primary' | 'outline' | 'secondary';
 }
 
 export const Button = (props: IButton) => {
   return (
-    <button className="px-5 py-2 flex place-content-center rounded-full bg-slate-500 text-black">
+    <button
+      className={`px-5 py-2 flex place-content-center rounded-full bg-slate-500 ${props.visual === 'primary' ? 'text-black' : 'text-red-500'}`}
+    >
       {props.children}
     </button>
   );
